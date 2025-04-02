6099bd8af8d7cf9fb7e4950966518c4e658353f2
Behzad <bh.ahmady@gmail.com>
2025-04-03 02:29:50 +0330

    Fix theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,20 @@
+import ThemeSwitcher from '@/app/Components/ThemeSwitcher'
 import Link from 'next/link'
 
-export default function Home() {
+const Home = () => {
   return (
     <div>
-      <nav className='bg-primary text-primary-content p-4 flex justify-between'>
-        <h1 className='text-2xl font-bold text underline decoration-amber-300'>
-          Daisyui
+      <nav className='bg-gray-600 text-primary-content p-4 flex justify-between'>
+        <h1 className='text-2xl font-bold underline decoration-amber-300'>
+          daisyUI
         </h1>
         <div className='flex gap-x-4'>
+          <ThemeSwitcher />
+
           <Link href='/' className='hover:text-base-100'>
             Home
           </Link>
+
           <Link href='/search' className='hover:text-base-100'>
             Search
           </Link>
@@ -21,3 +25,5 @@ export default function Home() {
     </div>
   )
 }
+
+export default Home
